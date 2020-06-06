ee72d02a51d904dd5d8dba4c909743953b51b655
Marc Partensky <marc.partensky@gmail.com>
2020-06-06 21:54:30 +0200

    you are useless

---
+++ alert-troll.sh
@@ -0,0 +1,10 @@
+function troll {
+        for i in {1..$2}
+        do  
+                sleep $1
+		echo $2
+		osascript -e 'display notification "you are useless"'
+                say "you are useless"
+        done
+}
+
