4b955f111c54db1a880d4ac8feda0b2e63507fa6
Marc Partensky <marc.partensky@gmail.com>
2020-06-06 22:09:38 +0200

    new

---
+++ youareuseless.sh
@@ -0,0 +1,11 @@
+function youareuseless {
+        for i in {1..$2}
+        do  
+                sleep $1
+                echo $2
+                osascript -e 'display notification "you are useless"'
+                say "you are useless"
+        done
+}
+
+youareuseless 1 3
