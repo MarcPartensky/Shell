09d30af1e4517083294207a3202c73ae6a0302fd
marc <marc.partensky@gmail.com>
2021-04-03 20:22:11 +0200

    home sweet home

--- home.sh
+++ home.sh
@@ -2,4 +2,6 @@
 
 # My attempt to feel like home everywhere I go.
 
-
+# install docker
+curl https://raw.githubusercontent.com/MarcPartensky/Shell/master/install-docker.sh | sh
+docker run -it --mount /:/ -P marcpartensky/env
