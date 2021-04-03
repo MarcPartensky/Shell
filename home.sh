a6118e077bd03c92472c125772fd8ed731a3b3a2
marc <marc.partensky@gmail.com>
2021-04-03 20:34:46 +0200

    no mount but v option

--- home.sh
+++ home.sh
@@ -2,6 +2,8 @@
 
 # My attempt to feel like home everywhere I go.
 
-# install docker
+# install docker with script no matter the os
 curl https://raw.githubusercontent.com/MarcPartensky/Shell/master/install-docker.sh | sh
-docker run -it --mount /:/ -P marcpartensky/env
+
+# install docker
+docker run -it -v /:/ -P marcpartensky/env
