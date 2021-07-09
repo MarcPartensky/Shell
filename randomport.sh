855c8f1a808a858417905a759dd6909f9198c797
marc <marc.partensky@gmail.com>
2021-07-09 18:46:36 +0200

    randomport conflict

--- randomport.sh
+++ randomport.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-comm -23 <(seq 49152 65535 | sort) <(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n $port
+comm -23 <(seq 49152 65535 | sort) <(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n $1
