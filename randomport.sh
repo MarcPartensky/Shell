501e9351cc53f76886fe90edd3d5d81a881030d8
marc <marc.partensky@gmail.com>
2021-07-09 18:50:55 +0200

    randport.sh

--- randomport.sh
+++ randomport.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-comm -23 <(seq 49152 65535 | sort) <(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n $1
+# comm -23 <(seq 49152 65535 | sort) <(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n $1
+ports=$(seq 49152 65535 | sort)
+a=$(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u)
+# | shuf | head -n $1
+comm -23 <($ports) <($a) | shuf | head -n $1
