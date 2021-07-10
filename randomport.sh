656b726d16ca2858c3e569e8a80023d60acf7585
marc <marc.partensky@gmail.com>
2021-07-10 08:30:11 +0000

    randomport.sh with bash not bourne shell

--- randomport.sh
+++ randomport.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# comm -23 <(seq 49152 65535 | sort) <(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n $1
-ports=$(seq 49152 65535 | sort)
-a=$(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u)
+randomport() {
+	comm -23 <(seq 49152 65535 | sort) <(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1
+}
+
+randomport
+# ports=$(seq 49152 65535 | sort)
+# a=$(ss -htan | awk '{print $4}' | cut -d':' -f2 | sort -u)
+# echo $a
+# echo $ports
 # | shuf | head -n $1
-comm -23 <($ports) <($a) | shuf | head -n $1
+# r=$(comm -23 <($ports) <($a))
+# echo $r
+
+# | shuf | head -n $1
