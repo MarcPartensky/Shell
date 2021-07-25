469f59d6a8cbccce6b9dd2b77b9f6a67ebc0f526
marc <marc.partensky@gmail.com>
2021-07-25 22:27:05 +0200

    restorevps

--- restorevps.sh
+++ restorevps.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-ssh tower ls  /srv/*07-25_15:51:20* | xargs -I {} scp ./{} vps:/srv
+echo "Restoration starting!"
+timestamp=$1
+towerpath="/media/ssd1/Marc/backups"
+vpspath="/srv"
+tmpfile="/tmp/vps_srv.txt"
+
+ssh tower ls $towerpath/*$timestamp* > $tmpfile
+echo "Transfering files"
+cat $tmpfile | xargs -I {} scp /{} vps:$vpspath/{}
+echo "Transfered files successfully"
+echo "Uncompressing files"
+ssh vps rename 's/_2021-07-25_15:51:20//' $vpspath/*.tar.gz
+ssh vps ls $vpspath > $tmpfile
+cat $tmpfile | xargs -I {} ssh vps tar xvzf $vpspath/{}
+echo "Uncompressed files"
+echo "Removing compressed files"
+cat $tmpfile | xargs -I {} rm $vpspath/{}
+echot "Removed compressed files"
+rm $tmpfile
+echo "Restoration complete!"
