6c06f551627d842657bbab8a60a0efe9bc9cee52
marc <marc.partensky@gmail.com>
2021-07-26 01:16:57 +0200

    restorevps that does not work

--- restorevps.sh
+++ restorevps.sh
@@ -1,22 +1,36 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 echo "Restoration starting!"
-timestamp=$1
+# timestamp=$1
+timestamp="2021-07-25_15:51:20"
 towerpath="/media/ssd1/Marc/backups"
 vpspath="/srv"
 tmpfile="/tmp/vps_srv.txt"
 
-ssh tower ls $towerpath/*$timestamp* > $tmpfile
+echo "timestamp: $timestamp"
+echo "towerpath: $towerpath"
+echo "vpspath: $vpspath"
+echo "tmpfile: $tmpfile"
+
+ssh tower ls -w 1 $towerpath | grep "$timestamp" > $tmpfile
+cat $tmpfile
 echo "Transfering files"
-cat $tmpfile | xargs -I {} scp /{} vps:$vpspath/{}
+# cat $tmpfile | xargs -I {} du -h tower:$towerpath/{}
+for f in $(cat $tmpfile); do
+	# echo "Transfering tower:$towerpath/$f"
+	# echo $f
+	echo "tower:/media/ssd1/Marc/backups/${f} vps:/srv"
+	scp tower:/media/ssd1/Marc/backups/${f} vps:/srv
+	# echo "Transfred $f"
+done
 echo "Transfered files successfully"
-echo "Uncompressing files"
-ssh vps rename 's/_2021-07-25_15:51:20//' $vpspath/*.tar.gz
-ssh vps ls $vpspath > $tmpfile
-cat $tmpfile | xargs -I {} ssh vps tar xvzf $vpspath/{}
-echo "Uncompressed files"
-echo "Removing compressed files"
-cat $tmpfile | xargs -I {} rm $vpspath/{}
-echot "Removed compressed files"
-rm $tmpfile
-echo "Restoration complete!"
+# echo "Uncompressing files"
+# ssh vps rename "s/_$timestamp//" $vpspath/*.tar.gz
+# ssh vps ls $vpspath > $tmpfile
+# cat $tmpfile | xargs -I {} ssh vps tar xvzf $vpspath/{}
+# echo "Uncompressed files"
+# echo "Removing compressed files"
+# cat $tmpfile | xargs -I {} rm $vpspath/{}
+# echot "Removed compressed files"
+# rm $tmpfile
+# echo "Restoration complete!"
