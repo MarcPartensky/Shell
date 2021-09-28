b6561d53f9fd82fdedcb4c667bc662b4d537c7bf
marc <marc.partensky@gmail.com>
2021-09-28 15:02:09 +0200

    gitrefresh

--- gitrefresh.sh
+++ gitrefresh.sh
@@ -1,14 +1,15 @@
-master=$1 || "master"
+#!/usr/bin/env zsh
+
+master=$([ "$#" -eq 1 ] && echo $1 || echo "master")
 branch=$(git branch --show-current)
-echo "Master:" $master "and branch:" $branch
-echo -e "\e[1mCheckout master\e[0m"
+echo -e "\e[1mCheckout $master\e[0m"
 git checkout $master
-echo -e "\e[1mPulling master\e[0m"
+echo -e "\e[1mPulling $master\e[0m"
 git pull
 echo -e "\e[1mCheckout $branch\e[0m"
 git checkout $branch
 echo -e "\e[1mMerging with $master\e[0m"
 git merge $master
-echo -e "\e[1mPushing\e[0m"
+echo -e "\e[1mPushing $branch\e[0m"
 git push
-echo Refreshed with $master"
+echo -e "\e[1mRefreshed $branch with $master\e[0m"
