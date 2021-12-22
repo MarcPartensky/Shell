02374cec7fcfbd6065ce04009dfce777709f6ad8
Marc Partensky <marc.partensky@gmail.com>
2021-12-22 23:17:47 +0100

    fixed symlink it works now

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@ for script in $(/bin/ls *.sh); do
         let COUNTER++
         cmd=$(echo $script | cut -d'.' -f1)
         echo -e "- \e[1m$script\e[0m as \e[1m$cmd\e[0m"
-        ln -sf ~/.local/bin/$cmd $file
+        ln -sf $PWD/$file ~/.local/bin/$cmd
     fi
 done
 echo -e "\e[3mLoaded $COUNTER commands\e[0m"
