c2dbce4bc6b04854dc62af79a0b19de34b7c1c04
Marc Partensky <marc.partensky@gmail.com>
2021-12-22 23:29:22 +0100

    script instead of file

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@ for script in $(/bin/ls); do
         let COUNTER++
         cmd=$(echo $script | cut -d'.' -f1)
         echo -e "- \e[1m$script\e[0m as \e[1m$cmd\e[0m"
-        ln -sf $PWD/$file ~/.local/bin/$cmd
+        ln -sf $PWD/$script ~/.local/bin/$cmd
     fi
 done
 echo -e "\e[3mLoaded $COUNTER commands\e[0m"
