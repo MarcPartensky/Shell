96c672b19159c2e10c7773867c9d4e2e8ad9c82a
Marc Partensky <marc.partensky@gmail.com>
2021-12-29 00:37:17 +0100

    sbin

--- routekiller.sh
+++ routekiller.sh
@@ -3,11 +3,11 @@
 # Remove invasive routing rules of vpns to be override those already defined
 
 echo Removed:
-ip route | grep 0.0.0.0/1 | while read line; do
+/sbin/ip route | grep 0.0.0.0/1 | while read line; do
     echo -e " - \033[1m$line\033[0m"
     sudo ip route del $line
 done
-ip route | grep 128.0.0.0/1 | while read line; do
+/sbin/ip route | grep 128.0.0.0/1 | while read line; do
     echo -e " - \033[1m$line\033[0m"
     sudo ip route del $line
 done
