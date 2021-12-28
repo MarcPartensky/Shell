36672754627bda0d27bde45027553bf9d8fb62e0
Marc Partensky <marc.partensky@gmail.com>
2021-12-28 23:53:31 +0100

    nice route killer

--- routekiller.sh
+++ routekiller.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
-invasiveroutes1=$(ip route | grep 0.0.0.0/1)
-invasiveroutes2=$(ip route | grep 128.0.0.0/1)
+# Remove invasive routing rules of vpns to be override those already defined
 
-invasiveroutes=( "${invasiveroutes1[@]}" "${invasiveroutes2[@]}" )
-echo ${invasiveroutes[@]}
+echo Removed :
+ip route | grep 0.0.0.0/1 | while read line; do
+    echo -e " - \033[1m$line\033[0m"
+    sudo ip route del $line
+done
+ip route | grep 128.0.0.0/1 | while read line; do
+    echo -e " - \033[1m$line\033[0m"
+    sudo ip route del $line
+done
