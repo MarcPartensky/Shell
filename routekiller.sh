d0d435b1b3796d37343bcda5e07b418defd88dee
Marc Partensky <marc.partensky@gmail.com>
2021-12-28 23:54:10 +0100

    removing invasive space

--- routekiller.sh
+++ routekiller.sh
@@ -2,7 +2,7 @@
 
 # Remove invasive routing rules of vpns to be override those already defined
 
-echo Removed :
+echo Removed:
 ip route | grep 0.0.0.0/1 | while read line; do
     echo -e " - \033[1m$line\033[0m"
     sudo ip route del $line
