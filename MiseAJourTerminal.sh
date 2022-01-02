aeab361177129850b11ad6471354457c9717ab2b
Marc Partensky <marc.partensky@gmail.com>
2022-01-03 00:57:30 +0100

    removed useless file

--- MiseAJourTerminal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip2 upgrade pip,numpy,theano,scipy,scikit-learn,jupiter,matplotlib,pandas,anaconda
-pip3 upgrade pip,numpy,theano,scipy,scikit-learn,jupiter,matplotlib,pandas,anaconda
