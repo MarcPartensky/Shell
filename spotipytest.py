abf89b0dea8bce701899db489a4b68cf22e04aa4
Marc Partensky <marc.partensky@gmail.com>
2022-01-16 16:20:12 +0100

    it works

--- spotipytest.py
+++ spotipytest.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python
 import os
-import sys
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 client_id = os.environ["SPOTIPY_CLIENT_ID"]
 client_secret = os.environ["SPOTIPY_CLIENT_SECRET"]
 print(client_id, client_secret)
 
 
-client_credentials_manager = SpotifyClientCredentials(
-    client_id=client_id,
-    client_secret=client_secret,
-)
-
 redirect_uri = "http://localhost:8888/callback/"
 
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=client_id,
-        client_secret=client_secret,
-        redirect_uri=redirect_uri,
-        scope="user-library-read",
-    )
+auth_manager=spotipy.oauth2.SpotifyOAuth(
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri=redirect_uri,
+    scope="user-library-read",
 )
 
-# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
 
 # playlists = sp.user_playlists("spotify")
 # while playlists:
@@ -40,7 +32,7 @@ sp = spotipy.Spotify(
 #     else:
 #         playlists = None
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results["items"]):
-    track = item["track"]
-    print(idx, track["artists"][0]["name"], " – ", track["name"])
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results["items"]):
+#     track = item["track"]
+#     print(idx, track["artists"][0]["name"], " – ", track["name"])
