065fd45faedb1f2b36d67bcc6d7637edb5254e48
Marc Partensky <marc.partensky@gmail.com>
2022-01-30 15:33:56 +0100

    better get metasploit

--- get-metasploit.sh
+++ get-metasploit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-> chmod 755 msfinstall && \
-> ./msfinstall
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > /tmp/msfinstall
+chmod 755 /tmp/msfinstall
+/tmp/msfinstall
+rm /tmp/msfinstall
+
