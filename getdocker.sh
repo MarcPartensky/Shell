f5868743ea7cd2a77ed9f32e942bdc5cab3ac4e6
Marc Partensky <marc.partensky@gmail.com>
2022-02-02 18:55:25 +0100

    getdocker

---
+++ getdocker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Get docker on debian
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Update
+sudo apt-get update
+
+# Install dependencies
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add docker official gpg key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Install docker engine
+ sudo apt-get update
+ sudo apt-get install docker-ce docker-ce-cli containerd.io
