9b65c5d50cd97a71d047ae748ddaed338456e6b8
Marc Partensky <marc.partensky@gmail.com>
2022-03-31 09:49:34 +0200

    pxssh

---
+++ pxsshtest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import getpass
+
+from pexpect import pxssh
+
+try:
+    s = pxssh.pxssh()
+    hostname = input("hostname: ")
+    username = input("username: ")
+    password = getpass.getpass("password: ")
+    s.login(hostname, username, password)
+    s.sendline("uptime")  # run a command
+    s.prompt()  # match the prompt
+    print(s.before.decode("utf-8"))  # print everything before the prompt.
+    s.sendline("ls -l")
+    s.prompt()
+    print(s.before)
+    print(s.before.decode("utf-8"))  # print everything before the prompt.
+    s.sendline("df")
+    s.prompt()
+    print(s.before)
+    print(s.before.decode("utf-8"))  # print everything before the prompt.
+    s.logout()
+except pxssh.ExceptionPxssh as e:
+    print("pxssh failed on login.")
+    print(e)
