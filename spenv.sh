4ea3d43195dfec497aa56bc4aa9c536fcedb5a53
Marc Partensky <marc.partensky@gmail.com>
2022-05-28 02:08:24 +0200

    dislike

--- spenv.sh
+++ spenv.sh
@@ -2,4 +2,5 @@
 
 source ~/git/secrets/spotipy.sh
 [[ $1 == 'splike' ]] && ~/.local/bin/splike ${@:2}
+[[ $1 == 'spdislike' ]] && ~/.local/bin/spdislike ${@:2}
 [[ $1 == 'spseek' ]] && ~/.local/bin/spseek ${@:2}
