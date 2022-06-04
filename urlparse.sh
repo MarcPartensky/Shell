0fdf95772be353a73f862c98ae99911e4c169fc6
Marc Partensky <marc.partensky@gmail.com>
2022-06-04 17:29:25 +0200

    urlparse + spawn-alacritty.sh

--- urlparse.sh
+++ urlparse.sh
@@ -2,27 +2,28 @@
 
 # extract the protocol
 proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+echo "proto: $proto"
 
 # remove the protocol -- updated
 url=$(echo $1 | sed -e s,$proto,,g)
+echo "url: $url"
 
 # extract the user (if any)
 user="$(echo $url | grep @ | cut -d@ -f1)"
+echo "user: $user"
 
 # extract the host and port -- updated
 hostport=$(echo $url | sed -e s,$user@,,g | cut -d/ -f1)
 
 # by request host without port
 host="$(echo $hostport | sed -e 's,:.*,,g')"
+echo "host: $host"
+
 # by request - try to extract the port
 port="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+echo "port: $port"
 
 # extract the path (if any)
 path="$(echo $url | grep / | cut -d/ -f2-)"
 
-echo "url: $url"
-echo "proto: $proto"
-echo "user: $user"
-echo "host: $host"
-echo "port: $port"
 echo "path: $path"
