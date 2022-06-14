d6419e5ce7f36484ff4facc3fc269e8696fce93d
Marc Partensky <marc.partensky@gmail.com>
2022-06-15 00:39:25 +0200

    fix lowbattery

--- lowbattery.sh
+++ lowbattery.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BATTINFO=`acpi -b | cut -c 12- | sed 's/%, /%\n/g' | sed 's/,/:/g'`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]]
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | tail -n 1 | cut -f 1 -d " "` < 00:15:00 ]]
 then
     DISPLAY=:0.0 /usr/bin/notify-send -u critical -t 10000 "Low battery" "$BATTINFO"
 fi
