13d9818e7f5660ee54aeecf193e41f38835d1961
Marc Partensky <marc.partensky@gmail.com>
2022-07-24 23:40:03 +0200

    freeze screen when screenshotting

--- screenshot.sh
+++ screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scrot ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
+scrot -f ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
