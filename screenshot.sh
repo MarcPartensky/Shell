2fe8e65e55514ad36e5103b88dfd2a555b4db4fd
Marc Partensky <marc.partensky@gmail.com>
2022-07-25 00:00:15 +0200

    doped screenshot with maim

--- screenshot.sh
+++ screenshot.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-scrot -f ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
+filepath=~/Pictures/`date +%Y-%m-%d_%H:%M:%S`.png
+maim -os $filepath
+cat $filepath | xclip -selection clipboard -t image/png
+notify-send "$filepath"
+# scrot -f ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
