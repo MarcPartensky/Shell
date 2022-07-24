d08fdc862d6319bc2a22e0b460fde766c779abc7
Marc Partensky <marc.partensky@gmail.com>
2022-07-25 00:07:16 +0200

    imgur too

--- screenshot.sh
+++ screenshot.sh
@@ -4,5 +4,5 @@ filepath=~/Pictures/`date +%Y-%m-%d_%H:%M:%S`.png
 maim -os $filepath
 cat $filepath | xclip -selection clipboard -t image/png
 notify-send -i $filepath $filepath
+notify-send -i $filepath `imgur.sh $filepath`
 # scrot -f ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
-# maim -os /tmp/screenshot.png && imgur.sh /tmp/screenshot.png | xclip -selection clipboard
