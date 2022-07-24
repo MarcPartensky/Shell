8109984bca2474bc736082f0cb59df7a98d55156
Marc Partensky <marc.partensky@gmail.com>
2022-07-25 00:03:22 +0200

    picture in notification

--- screenshot.sh
+++ screenshot.sh
@@ -3,5 +3,6 @@
 filepath=~/Pictures/`date +%Y-%m-%d_%H:%M:%S`.png
 maim -os $filepath
 cat $filepath | xclip -selection clipboard -t image/png
-notify-send "$filepath"
+notify-send -i $filepath $filepath
 # scrot -f ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
+# maim -os /tmp/screenshot.png && imgur.sh /tmp/screenshot.png | xclip -selection clipboard
