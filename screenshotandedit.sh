699b2fb1c4197f3c6dcf8fd988eac6fb615c27a5
Marc Partensky <marc.partensky@gmail.com>
2022-08-21 16:58:08 +0200

    wl-copy too

--- screenshotandedit.sh
+++ screenshotandedit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 filepath=~/Pictures/`date +%Y-%m-%d_%H:%M:%S`.png
-grim -g "$(slurp)" - | swappy -f - -o $filepath
+grim -g "$(slurp)" - | swappy -f - -o $filepath | wl-copy
 cat $filepath | xclip -selection clipboard -t image/png
 link=`imgur.sh $filepath`
 notify-send -i $filepath Screenshot "<a href=\"$link\">$link</a>"
