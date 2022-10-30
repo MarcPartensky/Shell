d136211841688c0cbef9c11e2c05a57f91cbf2ec
Marc Partensky <marc.partensky@gmail.com>
2022-10-30 03:16:24 +0100

    tilde partout

--- swwwnext.sh
+++ swwwnext.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 N=`/bin/ls ~/wallpapers | wc -l`
-swww img \
+~/.local/bin/swww img \
     -t grow \
     --transition-pos 0,1080 \
     ~/wallpapers/$(( $RANDOM % $N + 1 )).jpg
