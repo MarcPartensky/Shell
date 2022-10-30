7ce4dd99aa99dc47fbe481b604a46babf8b3c192
Marc Partensky <marc.partensky@gmail.com>
2022-10-30 02:05:07 +0100

    description

--- later.sh
+++ later.sh
@@ -3,7 +3,8 @@
 # Do some command and comeback later.
 name=`echo $1-$RANDOM`
 cmd=$@
+description="$name : $cmd"
 
-n "Started $name"
+n "Started $description"
 # abduco -n $name $SHELL -c "$SHELL -c '$cmd && n Finished $name'" &
-screen -dmS $name $SHELL -c "$cmd && n \"Finished $name\" && $SHELL"
+screen -dmS $name $SHELL -c "$cmd && n \"Finished $description\" && $SHELL"
