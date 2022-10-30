b68699b07d67bad090652537f06c78e3938665d2
Marc Partensky <marc.partensky@gmail.com>
2022-10-30 02:45:35 +0200

    later using screen

---
+++ later.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+# Do some command and comeback later.
+name=`echo $1-$RANDOM`
+cmd=$@
+
+n Started $name
+# abduco -n $name $SHELL -c "$SHELL -c '$cmd && n Finished $name'" &
+screen -dmS $name $SHELL -c "$cmd && n Finished $name && $SHELL"
