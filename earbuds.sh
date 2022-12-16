c6dfd693a57b290694bb36eb1ba6fe8ec2a95282
Marc Partensky <marc.partensky@gmail.com>
2022-12-17 00:53:11 +0100

    spenv

--- earbuds.sh
+++ earbuds.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+sudo systemctl restart bluetooth
 bluetoothctl connect 78:2B:64:F0:42:E7
--- spenv.sh
+++ spenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 source ~/git/secrets/spotipy.sh
-kill `lsof -i:49153 | tail -1 | awk '{print $2}'`
+lsof -i:49153 | tail -1 | awk '{print $2}' | xargs -I @ kill @
 ~/.local/bin/sp$1 ${@:2}
