6ac9b0a79cd38cd7d0fbcb36f2f2e23b1149cb10
Marc Partensky <marc.partensky@gmail.com>
2022-12-17 04:31:51 +0100

    lowercase

--- funding.sh
+++ funding.sh
@@ -3,7 +3,7 @@ folder=~/git
 
 for project in $folder/*/
 do
-    if git -C $project remote -v | grep marcpartensky > /dev/null
+    if git -C $project remote -v | tr '[A-Z]' '[a-z]' | grep marcpartensky > /dev/null
     then
         mkdir -p $project/.github
         cp $folder/template/.github/FUNDING.yml $project/.github/FUNDING.yml
