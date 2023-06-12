4a8ab9711e293a13764f7556efd4dfb5cc1233ae
Marc Partensky <marc.partensky@gmail.com>
2023-06-12 23:44:32 +0200

    simplest focalboard

--- focalboard.sh
+++ focalboard.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 DOCKER_HOST=unix:///run/docker.sock
 SERVICES_PATH=~/git/docker/services
+FOCALBOARD_ELECTRON_PATH=~/git/focalboard-electron
 
 docker compose -f $SERVICES_PATH/postgres.yml up -d postgres
 docker compose -f $SERVICES_PATH/focalboard.yml up -d focalboard
-firefox localhost:8000
+electron $FOCALBOARD_ELECTRON_PATH
+docker compose -f $SERVICES_PATH/postgres.yml rm -sf postgres
+docker compose -f $SERVICES_PATH/focalboard.yml rm -sf focalboard
