e71211e819f88f5e905d69294cb83a04a450c1ca
Marc Partensky <marc.partensky@gmail.com>
2023-06-13 21:43:04 +0200

    custom electron

--- focalboard.sh
+++ focalboard.sh
@@ -2,10 +2,11 @@
 
 # DOCKER_HOST=unix:///run/docker.sock
 SERVICES_PATH=~/git/docker/services
-FOCALBOARD_ELECTRON_PATH=~/git/focalboard-electron
+ELECTRON_PATH=~/git/custom-electron
+ELECTRON_HOST=localhost:8000
 
 docker compose -f $SERVICES_PATH/postgres.yml up -d postgres
 docker compose -f $SERVICES_PATH/focalboard.yml up -d focalboard
-electron $FOCALBOARD_ELECTRON_PATH
+HOST=$ELECTRON_HOST electron $ELECTRON_PATH
 docker compose -f $SERVICES_PATH/postgres.yml rm -sf postgres
 docker compose -f $SERVICES_PATH/focalboard.yml rm -sf focalboard
