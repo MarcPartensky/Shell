d51da5d5291c6b450d13f5e272ec40ff7d3a78a7
Marc Partensky <marc.partensky@gmail.com>
2023-06-13 21:46:16 +0200

    service

--- service.sh
+++ service.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+SERVICE=$1
+ELECTRON_HOST=$2
+
 # DOCKER_HOST=unix:///run/docker.sock
 SERVICES_PATH=~/git/docker/services
 ELECTRON_PATH=~/git/custom-electron
-ELECTRON_HOST=localhost:8000
 
 docker compose -f $SERVICES_PATH/postgres.yml up -d postgres
-docker compose -f $SERVICES_PATH/focalboard.yml up -d focalboard
+docker compose -f $SERVICES_PATH/$SERVICE.yml up -d $SERVICE
 HOST=$ELECTRON_HOST electron $ELECTRON_PATH
 docker compose -f $SERVICES_PATH/postgres.yml rm -sf postgres
-docker compose -f $SERVICES_PATH/focalboard.yml rm -sf focalboard
+docker compose -f $SERVICES_PATH/$SERVICE.yml rm -sf $SERVICE
