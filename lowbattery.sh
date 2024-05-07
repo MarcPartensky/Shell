acd9c901e55d4c616fa87e193e2130e89e92b507
Marc Partensky <marc.partensky@gmail.com>
2024-05-07 17:38:55 +0200

    lowbattery

--- lowbattery.sh
+++ lowbattery.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 BATTINFO=`acpi -b | cut -c 12- | sed 's/%, /%\n/g' | sed 's/,/:/g'`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | tail -n 1 | cut -f 1 -d " "` < 00:15:00 ]]
+timeleft=`echo $BATTINFO | tail -n 1 | cut -f 3 -d " "`
+echo $BATTINFO
+if [[ `echo $BATTINFO | grep Discharging` &&  $timeleft < 00:30:00 ]]
 then
     DISPLAY=:0.0 /usr/bin/notify-send -u critical -t 10000 "Low battery" "$BATTINFO"
 fi
