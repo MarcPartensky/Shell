d38102c2acea9f27e8afcba8ff139f94dd070d1f
Marc Partensky <marc.partensky@gmail.com>
2024-06-04 05:13:23 +0200

    screenshot log lik

--- screenshot.sh
+++ screenshot.sh
@@ -5,4 +5,5 @@ grim -g "$(slurp)" $filepath | wl-copy
 cat $filepath | xclip -selection clipboard -t image/png
 link=`imgur.sh $filepath`
 notify-send -i $filepath Screenshot "<a href=\"$link\">$link</a>"
+echo imgur: $link
 # scrot -f ~/Pictures/%Y-%m-%d_%H:%M:%S.png -s -e 'xclip -selection clipboard -target image/png -i $f'
