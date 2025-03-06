003a4f49570ead713b9f4f0804c7f5a088164b3a
Marc Partensky <marc.partensky@gmail.com>
2025-03-06 16:12:49 +0100

    smarter spenv that relies on playerctl

--- spenv.sh
+++ spenv.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
 source ~/git/secrets/spotipy.sh
+
+# search first player between spotify and spotube
+player=`playerctl -l | grep spot | head -1`
+cmds="play pause play-pause next prev"
+
+for cmd in $cmds
+do
+    if [ $1 = "$cmd" ]
+    then
+        playerctl -p $player $cmd
+        exit 0
+    fi
+done
+
 lsof -i:49153 | tail -1 | awk '{print $2}' | xargs -I @ kill @
 ~/.local/bin/sp$1 ${@:2}
