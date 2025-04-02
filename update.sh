18cc4262a4e065c0eb3974087c6f777c89c59924
Marc Partensky <marc.partensky@gmail.com>
2025-04-03 01:56:23 +0200

    update

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ sudo pacman -Sy
 sudo powerpill -Su
 paru -Su
 hyprpm update
+firefoxpwa profile list | g ID | awk '{print $2}' | xargs -I {} firefoxpwa profile update {}
 nvim --headless -c 'lua require("lazy").update()'
